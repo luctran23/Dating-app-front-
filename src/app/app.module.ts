a74b86b624b78e6b3f346cbf8145a3d9f7060e79
luctran23 <anhvanluc1999@gmail.com>
2020-10-11 22:03:03 +0700

    Generate girl component and test displaying data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GirlsComponent } from './girls/girls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GirlsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/models/girl.ts
@@ -0,0 +1,8 @@
+export interface Girl {
+    id: string;
+    name: string;
+    imgUrl: String;
+    age: Number;
+    hometown: String;
+    hobbies: String
+}
